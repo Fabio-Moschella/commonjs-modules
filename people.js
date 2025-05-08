12bbe8f36e840c440d41d6f54501588e7072b9a6
Fabio-Moschella <fabiomoschella1999@gmail.com>
2025-05-08 16:22:35 +0200

    add-function-objectfunction

--- people.js
+++ people.js
@@ -1,2 +1,9 @@
 const fullName = require("./name.js");
 const hobbies = require("./hobbies.js");
+const objectFunction = () => {
+  return {
+    fullname: fullName,
+    hobbies: hobbies,
+  };
+};
+console.log(objectFunction());
